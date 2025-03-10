2bd5d4acdd514d436d0f8393a22c28bd0862045b
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-11 01:19:48 +0600

    change pass with and without using old password

--- first_app/views.py
+++ first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from . forms import RegisterForm
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm
+# PasswordChangeForm => needs old password to change current password
+# SetPasswordForm => doesn't need old password to change current password
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 # Create your views here.
 
 def home(request):
@@ -51,3 +53,34 @@ def profile(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+# change password using old password
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user=request.user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                return redirect('profile')
+        else:
+            form = PasswordChangeForm(user=request.user)
+        return render(request, './passchange.html',{'form':form})
+    else:
+        return redirect('login')
+
+
+# change password without using old password
+def pass_change2(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SetPasswordForm(user=request.user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                return redirect('profile')
+        else:
+            form = SetPasswordForm(user=request.user)
+        return render(request, './passchange.html',{'form':form})
+    else:
+        return redirect('login')
